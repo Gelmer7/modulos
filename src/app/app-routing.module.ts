e630c6a0c17833d2954d1395c51857f9ef6e3a74
Gelmer Calla <gelmer.calla@iatec.com>
2020-12-13 19:01:23 -0300

    configuracion inicial funcionando
    modulos, about, navbar, progreso, home, rutas, angujar.json, instalacion de bootstrap, popper.js, jquery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProgresoComponent } from './components/progreso/progreso.component';
+
+const routes: Routes = [
+  { path:'home', component:HomeComponent},
+  { path: 'about', component:AboutComponent},
+  { path: 'progreso', component:ProgresoComponent},
+  { path: '', pathMatch:'full', redirectTo:'home'},
+  { path: '**', pathMatch:'full', redirectTo:'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProgresoComponent } from './components/progreso/progreso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    ProgresoComponent
   ],
   imports: [
     BrowserModule,
