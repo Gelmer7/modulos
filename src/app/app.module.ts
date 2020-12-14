480d41395cd8e14b1042bdb1645d64acb67b197d
Gelmer Calla <gelmer.calla@iatec.com>
2020-12-13 21:08:28 -0300

    botones mas y menos funcionando, imput necesita mejorar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -18,7 +20,8 @@ import { ProgresoComponent } from './components/progreso/progreso.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/progreso/progreso.component.ts
+++ src/app/components/progreso/progreso.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgresoComponent implements OnInit {
 
+  progreso:number = 50
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarValor(valor:number){
+
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100
+      return
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0
+      return
+    }
+
+    this.progreso = this.progreso + valor
+
   }
 
 }
